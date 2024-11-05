02b61169816732d32ffe983bf9f25cba72146288
Lameck Santos <lameck2@hotmail.com>
2024-11-05 18:00:54 -0300

    fix: Create Trouble plugin config

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -19,22 +19,6 @@ return {
   --},
 
   -- change trouble config
-  {
-    "folke/trouble.nvim",
-    -- opts will be merged with the parent spec
-    opts = { use_diagnostic_signs = true },
-  },
-
-  -- disable trouble
-  { "folke/trouble.nvim", enabled = true },
 
   -- override nvim-cmp and add cmp-emoji
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
 }
